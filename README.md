141e4c15f424637e8f08c01b96aa693dc6be00d8
Deepak Mane <deepak.mane123@gmail.com>
2022-06-23 23:51:31 -0700

    Create Login from

--- README.md
+++ README.md
@@ -36,5 +36,10 @@
 11. Save Cart Items
     1. install js-cookie package
     2. save and retrive cart items in cookies
+12. Create Login from
+    1. install react hook form
+    2. create input boxes
+    3. add login button
+
 
 
---
+++ pages/login.js
@@ -0,0 +1,69 @@
+import Link from 'next/link'
+import React from 'react'
+import Layout from '../components/Layout'
+import { useForm } from 'react-hook-form'
+
+export default function LoginScreen() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const submitHandler = ({ email, password }) => {
+    console.log('email : ', email, 'password : ', password);
+  }
+  return (
+    <Layout title='Login'>
+      <form
+        className='mx-auto max-w-screen-md'
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className='mb-4 text-xl'>Login</h1>
+        <div className='mb-4'>
+          <label htmlFor='email'>Email</label>
+          <input
+            {...register('email', {
+              required: 'Please enter email',
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Please enter valid email'
+              }
+            })}
+            type='email'
+            className='w-full'
+            id='email'
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className='text-red-500'>{errors.email.message}</div>
+          )}
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='password'>Password</label>
+          <input
+          {...register('password', {
+            required: 'Please enter password',
+            minLength: { value: 6 , message: 'Password is more than 5 chars'},
+          })}
+            type='password'
+            className='w-full'
+            id='password'
+            autoFocus
+          ></input>
+          {errors.password && (
+            <div className='text-red-500'>{errors.password.message}</div>
+          )}
+        </div>
+        <div className='mb-4'>
+          <button className='primary-button'>Login</button>
+        </div>
+        <div className='mb-4'>
+          Don&apos;t have an account? &nbsp;
+          <Link href='/register'>Register</Link>
+        </div>
+      </form>
+    </Layout>
+  )
+}
